a4a9bf721fadfd14daee04aa56c73a054d64159a
emiryucedag <emiryucedag@hotmail.com>
2024-03-07 12:20:17 +0300

    I have started my awesome projectgit statusgit status It will be great.

---
+++ src/main/java/com/mycompany/app/.idea/README.md
@@ -0,0 +1,1 @@
+This project is about the methods of "App.java". 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -3,6 +3,11 @@ package com.mycompany.app;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+
 
 /**
  * Unit test for simple App.
@@ -35,4 +40,63 @@ public class AppTest
     {
         assertTrue( true );
     }
+    public void testFound() {
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        assertTrue(new App().search(array, 4));
+    }
+
+    public void testNotFound() {
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        assertFalse(new App().search(array, 5));
+    }
+
+    public void testEmptyArray() {
+        ArrayList<Integer> array = new ArrayList<>();
+        assertFalse(new App().search(array, 1));
+    }
+
+    public void testNull() {
+        assertFalse(new App().search(null, 1));
+    }
+
+
+
+
 }
+/*
+*
+*  package com.mycompany.app;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+//import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+
+
+public class AppTest {
+    public void testFound() {
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        assertTrue(new com.mycompany.app.App().search(array, 4));
+    }
+
+    public void testNotFound() {
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        assertFalse(new com.mycompany.app.App().search(array, 5));
+    }
+
+    public void testEmptyArray() {
+        ArrayList<Integer> array = new ArrayList<>();
+        assertFalse(new com.mycompany.app.App().search(array, 1));
+    }
+
+    public void testNull() {
+        assertFalse(new com.mycompany.app.App().search(null, 1));
+    }
+
+}
+
+*
+* */
