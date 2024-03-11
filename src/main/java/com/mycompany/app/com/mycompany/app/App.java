b0f618c057ad4e1e7036e208aa5b4f03ee133799
emiryucedag <emiryucedag@hotmail.com>
2024-03-11 21:33:19 +0300

    The method inside App.java and its tests are updated.

--- src/main/java/com/mycompany/app/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/com/mycompany/app/App.java
@@ -5,21 +5,45 @@ import java.util.ArrayList;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
-    public static boolean search(ArrayList<Integer> array, int e) {
-        System.out.println("inside search");
-        if (array == null) return false;
 
-        for (int elt : array) {
-            if (elt == e) return true;
+    public static int addAndMultiply(ArrayList<Integer> arrayList, int[] array, String str1, String str2) {
+        System.out.println("inside of method");
+        if (arrayList == null) return 0;
+        if (array == null) return 0;
+
+        int sumOfArrayList = 0;
+        int sumOfArray = 0;
+
+        // ArrayList<Integer> ve int[] parametrelerinin toplamını bul
+        for (Integer num : arrayList) {
+            sumOfArrayList += num;
         }
-        return false;
-    }
 
+        for (Integer num : array) {
+            sumOfArray += num;
+        }
+
+        int firstNumber,secondNumber;
+
+        if (str1!=null)
+            firstNumber= str1.length()*sumOfArrayList;
+        else
+            firstNumber= 0;
+
+        if (str2!=null)
+            secondNumber=str2.length()*sumOfArray;
+        else
+            secondNumber= 0;
+
+
+
+        int answer= firstNumber+secondNumber;
+        System.out.println("the answer is: "+answer);
+        return answer;
+    }
 }
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -40,24 +40,68 @@ public class AppTest
     {
         assertTrue( true );
     }
-    public void testFound() {
-        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        assertTrue(new App().search(array, 4));
+    public void testAddAndMultiply() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(5);
+        arrayList.add(10);
+        int[] array = {2,4};
+        String str1= "hello";
+        String str2= "hi";
+
+        //method doğru mu çalışıyor yanlış mı kontrolü
+        assertEquals(87,new App().addAndMultiply(arrayList,array,str1,str2));
     }
 
-    public void testNotFound() {
-        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        assertFalse(new App().search(array, 5));
+    public void testEmptyArrayList() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int[] array = {2,4};
+        String str1= "hello";
+        String str2= "hi";
+
+        //boş array listte 0 dönecek mi testi
+        assertEquals(12,new App().addAndMultiply(arrayList,array,str1,str2));
     }
 
     public void testEmptyArray() {
-        ArrayList<Integer> array = new ArrayList<>();
-        assertFalse(new App().search(array, 1));
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(5);
+        arrayList.add(10);
+        int[] array = new int[5];
+
+        String str1= "hello";
+        String str2= "hi";
+
+        //boş arrayde 0 dönecek mi testi
+        assertEquals(75,new App().addAndMultiply(arrayList,array,str1,str2));
+
     }
 
-    public void testNull() {
-        assertFalse(new App().search(null, 1));
+    public void testEmptyStrings() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(5);
+        arrayList.add(10);
+        int[] array = {2,4};
+        String str1= "";
+        String str2= "";
+
+        //method doğru mu çalışıyor yanlış mı kontrolü
+        assertEquals(0,new App().addAndMultiply(arrayList,array,str1,str2));
     }
+
+    public void testNullStrings() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(5);
+        arrayList.add(10);
+        int[] array = {2,4};
+        String str1= null;
+        String str2= null;
+
+        //method doğru mu çalışıyor yanlış mı kontrolü
+        assertEquals(0,new App().addAndMultiply(arrayList,array,str1,str2));
+
+    }
+
+
 
 
 
