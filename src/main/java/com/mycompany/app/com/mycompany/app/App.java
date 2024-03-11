90cbbe8e8dc7eea0737d7240116d3c1a878f6fd1
emiryucedag <emiryucedag@hotmail.com>
2024-03-11 22:55:06 +0300

    Getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/com/mycompany/app/App.java
@@ -1,5 +1,18 @@
 package com.mycompany.app;
+
+
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 
 /**
  * Hello world!
@@ -7,8 +20,66 @@ import java.util.ArrayList;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+            //System.out.println(req.queryParams("input1"));
+            //System.out.println(req.queryParams("input2"));
+
+            String input1 = req.queryParams("input1");
+            java.util.Scanner sc1 = new java.util.Scanner(input1);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+                inputList.add(value);
+            }
+            System.out.println(inputList);
+
+
+            String input2 = req.queryParams("input2").replaceAll("\\s","");
+            int input2AsInt = Integer.parseInt(input2);
+
+            boolean result ;
+
+            int[] intArray = new int[inputList.size()];
+            for (int i = 0; i < inputList.size(); i++) {
+                intArray[i] = inputList.get(i); // ArrayList'ten her bir öğeyi int dizisine kopyala
+            }
+
+            int answer=App.addAndMultiply(inputList, intArray,"hello","world");
+
+            if (answer==0) result=false;
+                    else result=true;
+
+            Map map = new HashMap();
+            map.put("result", result);
+            return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+                (rq, rs) -> {
+                    Map map = new HashMap();
+                    map.put("result", "not computed yet!");
+                    return new ModelAndView(map, "compute.mustache");
+                },
+                new MustacheTemplateEngine());
     }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+
+
 
     public static int addAndMultiply(ArrayList<Integer> arrayList, int[] array, String str1, String str2) {
         System.out.println("inside of method");
